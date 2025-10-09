7f1d46caecaf65059eba48dd53459f20b6f148ae
ivannofick <ivannofick@gmail.com>
2025-10-09 17:47:46 +0700

    titip dulu

--- app-next/next.config.ts
+++ app-next/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: '/app-next',
+  assetPrefix: process.env.NODE_ENV === "production" ? "/app-next" : undefined,
   // output: 'standalone',
 };
 
