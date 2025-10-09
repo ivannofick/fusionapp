cbd9a77bc7f95582f4a9e0ffe7ec8fd636004cc4
ivannofick <ivannofick@gmail.com>
2025-10-09 15:49:34 +0700

    fix lah pusing

--- app-next/next.config.ts
+++ app-next/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // basePath: '/app-next',
+  // output: 'standalone',
 };
 
 export default nextConfig;
